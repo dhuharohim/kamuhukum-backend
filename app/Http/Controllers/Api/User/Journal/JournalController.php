bca7f5a728d384e9a47d58b4deea9fb0ad19f21f
dhuha <dhuhacmd@gmail.com>
2024-08-08 02:25:18 +0700

    Revert "update"
    This reverts commit e0d81d717d3120372dee774b58e667f85f43c539.

--- app/Http/Controllers/Api/User/Journal/JournalController.php
+++ app/Http/Controllers/Api/User/Journal/JournalController.php
@@ -262,13 +262,14 @@ class JournalController extends Controller
             foreach ($request->article_files as $index => $fileData) {
                 $path = 'failed';
                 if ($request->hasFile("article_files.$index.file")) {
-                    $path = $request->file("article_files.$index.file")->storeAs('uploads/articles/' . $article->article_for, $fileData['name'], 'sftp');
+                    $path = $request->file("article_files.$index.file")->storeAs('uploads/articles/' . $article->article_for, $fileData['name'], 'public');
+                    $url = Storage::url($path);
                 }
 
                 ArticleFile::create([
                     'article_id' => $article->id,
                     'file_name' => $fileData['name'],
-                    'file_path' => $path,
+                    'file_path' => $url,
                     'type' => $fileData['type']
                 ]);
             }
