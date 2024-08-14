e3fcbc49a4d70382e8603bbf322803c3d47663f6
dhuha <dhuhacmd@gmail.com>
2024-08-14 23:09:35 +0700

    update

--- resources/views/Contents/edition/show.blade.php
+++ resources/views/Contents/edition/show.blade.php
@@ -5,16 +5,16 @@
 @endsection
 
 @section('page_content')
-    <div class="alert alert-warning" role="alert">
-        <span class="nftmax-menu-icon nftmax-svg-icon__v9 me-1">
-            <svg class="nftmax-svg-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd"
-                    d="M18.458 3.11A1 1 0 0 1 19 4v16a1 1 0 0 1-1.581.814L12 16.944V7.056l5.419-3.87a1 1 0 0 1 1.039-.076ZM22 12c0 1.48-.804 2.773-2 3.465v-6.93c1.196.692 2 1.984 2 3.465ZM10 8H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6V8Zm0 9H5v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3Z"
-                    clip-rule="evenodd" />
-            </svg>
-        </span>
-        @if ($edition->announcement)
+    @if ($edition->announcement)
+        <div class="alert alert-warning" role="alert">
+            <span class="nftmax-menu-icon nftmax-svg-icon__v9 me-1">
+                <svg class="nftmax-svg-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M18.458 3.11A1 1 0 0 1 19 4v16a1 1 0 0 1-1.581.814L12 16.944V7.056l5.419-3.87a1 1 0 0 1 1.039-.076ZM22 12c0 1.48-.804 2.773-2 3.465v-6.93c1.196.692 2 1.984 2 3.465ZM10 8H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6V8Zm0 9H5v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-3Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </span>
             <a class="alert-link fw-bold" href="{{ route('announcements.show', $edition->announcement->id) }}">
                 <u>{{ $edition->announcement->title }}</u> </a>
 
@@ -24,8 +24,9 @@
                 <span>, and extended until
                     {{ date('d M Y', strtotime($edition->announcement->extend_submission_date)) }}</span>
             @endif
-        @endif
-    </div>
+        </div>
+    @endif
+
     <div class="welcome-cta mg-top-40 d-block">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
